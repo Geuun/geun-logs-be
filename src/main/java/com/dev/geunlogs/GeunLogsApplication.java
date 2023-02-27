b0e644b11071c2456ff35b8478ec26fa4ce11ecf
Geuun <jgk9612@gmail.com>
2023-02-28 02:29:15 +0900

    Initial commit
    backend initial commit
    with (springboot, security, postgresql)

---
+++ src/main/java/com/dev/geunlogs/GeunLogsApplication.java
@@ -0,0 +1,13 @@
+package com.dev.geunlogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeunLogsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeunLogsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/geunlogs/GeunLogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.geunlogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeunLogsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
